be13bb8aa576d2399765449cd2b761029258e0a5
shota-tabaghua <82117052+shota-tabaghua@users.noreply.github.com>
2021-05-20 13:41:12 +0400

    add slick slider

--- scirpt.js
+++
@@ -1,7 +0,0 @@
-$(function(){
-    $('.multiple-items').slick({
-     infinite: true,
-     slidesToShow: 1,
-     slidesToScroll: 1,
-     arrows: true,
-     dots:true,
---
+++ script.js
@@ -0,0 +1,25 @@
+$(function(){
+    $('.multiple-items').slick({
+     infinite: true,
+     slidesToShow: 3,
+     slidesToScroll: 1,
+     arrows: true,
+     dots:true,
+     responsive: [
+         {
+           breakpoint: 768,
+           settings: {
+             slidesToShow: 2,
+             slidesToScroll: 1
+           }
+         },
+         {
+           breakpoint: 480,
+           settings: {
+             slidesToShow: 1,
+             slidesToScroll: 1
+           }
+         }
+     ]
+   });    
+ });
